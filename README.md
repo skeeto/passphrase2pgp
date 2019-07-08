c85000aa7774a14cbd973009dbd1551c5a3a2209
Christopher Wellons <wellons@nullprogram.com>
2019-07-08 01:24:51 -0400

    Also generate an encryption cv25519 subkey

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 # Predictable, passphrase-based PGP key generator
 
-passphrase2pgp generates an EdDSA OpenPGP key entirely from a
-passphrase, essentially allowing you to store a backup of your PGP key
-in your brain. At any time you can re-run the tool and re-enter the
-passphrase to reproduce the original key.
+passphrase2pgp generates, in OpenPGP format, an EdDSA signing key and
+Curve25519 encryption subkey entirely from a passphrase, essentially
+allowing you to store a backup of your PGP keys in your brain. At any
+time you can re-run the tool and re-enter the passphrase to reproduce
+the original keys.
 
-The key is derived from the passphrase using [Argon2id][argon2]
+The keys are derived from the passphrase using [Argon2id][argon2]
 (memory=1GB and time=8) and [RFC 8032][rfc8032]. It's aggressive enough
 to protect passphrases short enough to be memorable by humans from
 offline brute force attacks. Always use [a strong passphrase][dw].
-
-passphrase2pgp only generates an EdDSA (e.g. sign only) primary key. The
-primary key is not encrypted (no string-to-key), and no subkeys are
-generated. The intended purpose is to recover, without backups, a
-cryptographic identity, not necessarily encryption keys.
 
 [argon2]: https://github.com/P-H-C/phc-winner-argon2
 [rfc8032]: https://tools.ietf.org/html/rfc8032
@@ -60,61 +56,19 @@ passphrase on your GnuPG keychain in order to protect it at rest:
     $ gpg --edit-key "Real Name"
     gpg> passwd
 
-If you want to use GnuPG for encryption, create an encryption subkey
-associated with your passphrase-backed identity:
+## Philosophy
 
-    $ gpg --expert --edit-key "Real Name"
-    gpg> addkey
-    Please select what kind of key you want:
-       (3) DSA (sign only)
-       (4) RSA (sign only)
-       (5) Elgamal (encrypt only)
-       (6) RSA (encrypt only)
-       (7) DSA (set your own capabilities)
-       (8) RSA (set your own capabilities)
-      (10) ECC (sign only)
-      (11) ECC (set your own capabilities)
-      (12) ECC (encrypt only)
-      (13) Existing key
-    Your selection? 12
-    Please select which elliptic curve you want:
-       (1) Curve 25519
-       (3) NIST P-256
-       (4) NIST P-384
-       (5) NIST P-521
-       (6) Brainpool P-256
-       (7) Brainpool P-384
-       (8) Brainpool P-512
-       (9) secp256k1
-    Your selection? 1
-    Please specify how long the key should be valid.
-             0 = key does not expire
-          <n>  = key expires in n days
-          <n>w = key expires in n weeks
-          <n>m = key expires in n months
-          <n>y = key expires in n years
-    Key is valid for? (0)
-    Key does not expire at all
-    Is this correct? (y/N) y
-    Really create? (y/N) y
+Since [OpenPGP encryption is neither good nor useful anymore][mg], I
+considered not generating an encryption subkey. The "privacy" portion of
+OpenPGP has become the least important part.
 
-Warning: **You will not be able to recover subkeys since they are not
-derived from the passphrase.**
-
-## Roadmap
-
-Perhaps passphrase2pgp should also generate an encryption subkey (Curve
-25519) at the same time? This would allow subkeys to be recovered as
-well. However, [OpenPGP encryption is neither good nor useful
-anymore][mg], and I'd prefer not to encourage its use.
-
-OpenPGP signatures still have *some* limited use, mostly due to momentum
-and lack of alternatives. The OpenPGP specification is over-engineered,
-loaded with useless legacy cruft, and ambiguous in many places. GnuPG is
-honestly not that great of an OpenPGP implementation, and I have low
-confidence in it. I stubbed my toe on a number of minor GnuPG bugs when
-hammering it with (usually invalid) output from passphrase2pgp while it
-was being developed.
+OpenPGP digital signatures still have *some* limited use, mostly due to
+momentum and lack of alternatives. The OpenPGP specification is
+over-engineered, loaded with useless legacy cruft, and ambiguous in many
+places. GnuPG is honestly not that great of an OpenPGP implementation,
+and I have low confidence in it. I stubbed my toe on a number of minor
+GnuPG bugs when hammering it with (usually invalid) output from
+passphrase2pgp while it was being developed.
 
 [mg]: https://blog.cryptographyengineering.com/2014/08/13/whats-matter-with-pgp/
 
--- passphrase2pgp.go
+++ passphrase2pgp.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"golang.org/x/crypto/argon2"
+	"golang.org/x/crypto/curve25519"
 	"golang.org/x/crypto/ed25519"
 	"golang.org/x/crypto/ssh/terminal"
 )
@@ -71,7 +72,8 @@ func readPassphrase(repeat int) ([]byte, error) {
 func kdf(passphrase []byte, time uint32) []byte {
 	var memory uint32 = 1024 * 1024 // 1 GB
 	var threads uint8 = 1
-	return argon2.IDKey(passphrase, nil, time, memory, threads, 32)
+	var seedSize uint32 = ed25519.SeedSize
+	return argon2.IDKey(passphrase, nil, time, memory, threads, seedSize)
 }
 
 // Returns a Secret-Key Packet for a key pair.
@@ -129,8 +131,7 @@ func newUserIDPacket(uid string) []byte {
 }
 
 // Returns a Signature Packet binding a Secret-Key Packet and User ID Packet.
-func newSignaturePacket(key ed25519.PrivateKey, skpacket, idpacket []byte,
-	created int64) []byte {
+func newKeySignaturePacket(key ed25519.PrivateKey, skpacket, idpacket []byte, created int64) []byte {
 
 	keyid := keyid(skpacket)
 	sigpacket := []byte{
@@ -182,12 +183,143 @@ func newSignaturePacket(key ed25519.PrivateKey, skpacket, idpacket []byte,
 	return sigpacket
 }
 
+func newSecretSubkeyPacket(seckey, pubkey []byte, created int64) []byte {
+	packet := []byte{
+		0xc7, // packet header, new format, Secret-Subkey Packet (7)
+		0,    // packet length
+		0x04, // packet version, new (4)
+
+		// Public Key
+		// creation date
+		byte(created >> 24),
+		byte(created >> 16),
+		byte(created >> 8),
+		byte(created >> 0),
+		18, // algorithm, Elliptic Curve
+		10, // OID length
+		// OID (1.3.6.1.4.1.3029.1.5.1)
+		0x2b, 0x06, 0x01, 0x04, 0x01, 0x97, 0x55, 0x01, 0x05, 0x01,
+		// public key length (always 263 bits)
+		0x01, 0x07,
+		0x40, // MPI prefix
+		// public key (32 bytes)
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		// KDF parameters
+		3,    // length
+		0x01, // reserved (1)
+		0x08, // SHA-256
+		0x07, // AES-128? (spec is incorrect)
+
+		// Secret Key
+		0, // string-to-key, unencrypted
+	}
+	copy(packet[22:54], pubkey)
+
+	// append MPI-encoded key
+	secmpi := mpi(reverse(seckey))
+	packet = append(packet, secmpi...)
+
+	// compute and append checksum
+	var checksum uint16
+	for _, b := range secmpi {
+		checksum += uint16(b)
+	}
+	packet = append(packet, []byte{
+		byte(checksum >> 8), byte(checksum >> 0),
+	}...)
+
+	packet[1] = byte(len(packet) - 2)
+	return packet
+}
+
 // Returns the Key ID from a Secret-Key Packet.
 func keyid(skpacket []byte) []byte {
 	h := sha1.New()
 	h.Write([]byte{0x99, 0, 51}) // "packet" length = 51
 	h.Write(skpacket[2:53])      // public key portion
 	return h.Sum(nil)
+}
+
+// Return the Curve25519 public key for a secret key.
+func x25519(seckey []byte) []byte {
+	var xpubkey [32]byte
+	var xseckey [32]byte
+	copy(xseckey[:], seckey)
+	curve25519.ScalarBaseMult(&xpubkey, &xseckey)
+	return xpubkey[:]
+}
+
+// Return a reversed copy.
+func reverse(b []byte) []byte {
+	c := make([]byte, len(b))
+	for i, v := range b {
+		c[len(c)-i-1] = v
+	}
+	return c
+}
+
+// Return a Curve25519 keypair from a seed.
+func newCurve25519Keys(seed []byte) (seckey, pubkey []byte) {
+	h := sha256.New()
+	h.Write(seed)
+	seckey = h.Sum(nil)
+	seckey[0] &= 248
+	seckey[31] &= 127
+	seckey[31] |= 64
+	pubkey = x25519(seckey)
+	return
+}
+
+func newSubkeySignaturePacket(key ed25519.PrivateKey, skpacket, sskpacket []byte, created int64) []byte {
+	keyid := keyid(skpacket)
+	sigpacket := []byte{
+		0xc2,  // packet header, new format, Signature Packet (2)
+		0,     // packet length
+		0x04,  // packet version, new (4)
+		0x18,  // signature type, Subkey Binding Signature
+		22,    // public-key algorithm, EdDSA
+		8,     // hash algorithm, SHA-256
+		0, 16, // hashed subpacket data length
+		// Signature Creation Time subpacket (length=5, type=2)
+		5, 2,
+		byte(created >> 24),
+		byte(created >> 16),
+		byte(created >> 8),
+		byte(created >> 0),
+		// Issuer subpacket (length=9, type=16)
+		9, 16,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, // no unhashed subpacket data
+		0, 0, // hash value preview
+	}
+
+	// Fill out Issuer subpacket
+	copy(sigpacket[16:24], keyid[12:])
+
+	// Compute digest to be signed
+	h := sha256.New()
+	h.Write([]byte{0x99, 0, 51})
+	h.Write(skpacket[2:53]) // public key portion
+	h.Write([]byte{0x99, 0, 56})
+	h.Write(sskpacket[2:58])
+	h.Write(sigpacket[2:24])              // trailer
+	h.Write([]byte{4, 0xff, 0, 0, 0, 22}) // final trailer
+	sigsum := h.Sum(nil)
+	sig := ed25519.Sign(key, sigsum)
+
+	// Fill out hash preview
+	sigpacket[26] = sigsum[0]
+	sigpacket[27] = sigsum[1]
+
+	// Fill out signature
+	r := sig[:32]
+	sigpacket = append(sigpacket, mpi(r)...)
+	m := sig[32:]
+	sigpacket = append(sigpacket, mpi(m)...)
+
+	sigpacket[1] = byte(len(sigpacket)) - 2
+	return sigpacket
 }
 
 func main() {
@@ -231,6 +363,14 @@ func main() {
 	os.Stdout.Write(idpacket)
 
 	// Signature Packet
-	sigpacket := newSignaturePacket(key, skpacket, idpacket, *created)
+	sigpacket := newKeySignaturePacket(key, skpacket, idpacket, *created)
 	os.Stdout.Write(sigpacket)
+
+	// Secret-Subkey Packet
+	subseckey, subpubkey := newCurve25519Keys(seed)
+	sskpacket := newSecretSubkeyPacket(subseckey, subpubkey, *created)
+	os.Stdout.Write(sskpacket)
+
+	ssigpacket := newSubkeySignaturePacket(key, skpacket, sskpacket, *created)
+	os.Stdout.Write(ssigpacket)
 }
