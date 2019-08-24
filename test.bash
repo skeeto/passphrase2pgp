cb58332547824f742a1dc58d667cf546ee9a83e4
Christopher Wellons <wellons@nullprogram.com>
2019-08-24 16:12:44 -0400

    Add test.bash to test against GnuPG and OpenSSH

---
+++ test.bash
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# This bash script tests the outputs of passphrase2pgp against both
+# GnuPG and OpenSSH. You will need the go, gpg, gpgv, and ssh-keygen
+# commands on your path before running this script.
+
+set -euo pipefail
+
+export REALNAME="John Doe"
+export EMAIL="john.doe@example.com"
+export KEYID="2536A19C9C54880A8FEBC812070B00717FCDEE34"
+passphrase="foobar"
+
+go test
+go build
+
+homedir=$(mktemp -d homedir.XXXXXX)
+chmod 700 $homedir
+cleanup() {
+    rm -rf $homedir
+}
+trap cleanup INT TERM EXIT
+gpg="gpg --quiet --homedir $homedir"
+gpgv="gpgv --quiet --homedir $homedir"
+
+echo === Testing SSH Keys ===
+./passphrase2pgp -K --uid doe@exmaple.com \
+                    --check '' \
+                    --format ssh \
+                    --input <(echo $passphrase) \
+                    --protect | \
+    (umask 077; tee $homedir/id_ed25519)
+ssh-keygen -y -P $passphrase -f $homedir/id_ed25519 | \
+    tee $homedir/id_ed25519.pub
+./passphrase2pgp -K --uid john@exmaple.com \
+                    --check '' \
+                    --format ssh \
+                    --input <(echo $passphrase) | \
+    (umask 077; tee $homedir/id_ed25519x)
+ssh-keygen -y -P '' -f $homedir/id_ed25519x | \
+    tee $homedir/id_ed25519x.pub
+
+echo === Testing Unprotected PGP Keys ===
+./passphrase2pgp -K --input <(echo $passphrase) \
+                    --armor | \
+    tee $homedir/seckey.asc
+./passphrase2pgp -K --load $homedir/seckey.asc \
+                    --public \
+    > $homedir/trustedkeys.kbx
+
+echo === Testing PGP Signatures ===
+echo hello | \
+    tee /dev/stderr | \
+    ./passphrase2pgp -T --load $homedir/seckey.asc | $gpgv
+echo message > $homedir/message
+./passphrase2pgp -S --load $homedir/seckey.asc $homedir/message
+$gpgv $homedir/message.sig $homedir/message
+./passphrase2pgp -S --load $homedir/seckey.asc --armor $homedir/message
+$gpgv $homedir/message.asc $homedir/message
+
+echo === Testing Protected PGP Keys ===
+./passphrase2pgp -K --input <(echo $passphrase) \
+                    --protect \
+                    --armor \
+    | tee $homedir/seckey.s2k.asc
+$gpg --passphrase-file <(echo $passphrase) \
+     --pinentry-mode loopback \
+     --import $homedir/seckey.s2k.asc
+
+echo === All Tests Passed ===
